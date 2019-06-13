5587a7c8b98efdc93d13c671facb8e942ee59ff9
Nathymedina2305 <natalia.juliano@hotmail.com>
2019-06-13 20:29:45 -0300

    completo site

--- meusite/app.js
+++ meusite/app.js
@@ -4,7 +4,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usuariosRouter = require('./routes/usuarios');
+var leiturasRouter = require('./routes/leituras');
 
 var app = express();
 
@@ -15,6 +16,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/usuarios', usuariosRouter);
+app.use('/leituras', leiturasRouter);
 
 module.exports = app;
